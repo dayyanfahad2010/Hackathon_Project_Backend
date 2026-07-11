f89de64b35393a34843e305ab8bff154bbc06cd0
dayyanfahad2010 <dayyanfahad20@gmail.com>
2026-07-11 15:05:36 +0500

    Cors Updated

--- src/app.js
+++ src/app.js
@@ -6,7 +6,18 @@ import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://hackathon-project-sigma-three.vercel.app",
+  ], 
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.options('/{*path}', cors(corsOptions)); 
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 app.get("/",(req,res)=>{
